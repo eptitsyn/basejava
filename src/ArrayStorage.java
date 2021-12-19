46e1fbd3e7175dd1d32ef43b1687026eeeb73337
Evgenii Ptitsyn <eptitsyn@gmail.com>
2021-12-19 15:56:21 +0300

    ititial commit

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,50 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int count;
 
     void clear() {
+        for (int i = 0; i < count; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[count] = r;
+        count++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < count; i++) {
+            if (storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < count; i++) {
+            if (storage[i].uuid == uuid) {
+                storage[i] = storage[count - 1];
+                storage[count - 1] = null;
+                count--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, count);
     }
 
     int size() {
-        return 0;
+        return count;
     }
 }
