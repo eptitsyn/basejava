c48b1c7790b1f9bed5394f43446bab8be2a82912
Evgenii Ptitsyn <eptitsyn@gmail.com>
2022-03-01 22:00:41 +0300

    hw4 second issues fix

--- test/com/eptitsyn/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/eptitsyn/webapp/storage/AbstractArrayStorageTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 abstract class AbstractArrayStorageTest {
     protected static final int EXPECTED_SIZE = 5;
     protected Storage storage;
-    protected Resume[] testResume = new Resume[EXPECTED_SIZE];
+    protected Resume[] expectedResumes = new Resume[EXPECTED_SIZE];
 
     public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
@@ -28,7 +28,7 @@ abstract class AbstractArrayStorageTest {
         for (int i = 0; i < EXPECTED_SIZE; i++) {
             Resume r = new Resume();
             storage.save(r);
-            testResume[i] = r;
+            expectedResumes[i] = r;
         }
     }
 
@@ -41,8 +41,8 @@ abstract class AbstractArrayStorageTest {
 
     @Test
     void update() {
-        storage.update(testResume[3]);
-        assertSame(testResume[3], storage.get(testResume[3].getUuid()));
+        storage.update(expectedResumes[3]);
+        assertSame(expectedResumes[3], storage.get(expectedResumes[3].getUuid()));
     }
 
     @Test
@@ -55,18 +55,16 @@ abstract class AbstractArrayStorageTest {
         Resume r = new Resume();
         storage.save(r);
         assertEquals(EXPECTED_SIZE + 1, storage.size());
-        assertNotNull(storage.get(r.getUuid()));
+        assertSame(storage.get(r.getUuid()), r);
     }
 
     @Test
     void saveExists() {
-        Resume r = new Resume();
-        storage.save(r);
-        assertThrows(ExistStorageException.class, () -> storage.save(r));
+        assertThrows(ExistStorageException.class, () -> storage.save(expectedResumes[0]));
     }
 
     @Test
-    void saveLimit() {
+    void saveOverflow() {
         assertDoesNotThrow(() -> {
             for (int i = 0; i < (STORAGE_LIMIT - EXPECTED_SIZE); i++) {
                 storage.save(new Resume());
@@ -77,7 +75,7 @@ abstract class AbstractArrayStorageTest {
 
     @Test
     void get() {
-        assertSame(storage.get(testResume[3].getUuid()), testResume[3]);
+        assertSame(storage.get(expectedResumes[3].getUuid()), expectedResumes[3]);
     }
 
     @Test
@@ -87,8 +85,9 @@ abstract class AbstractArrayStorageTest {
 
     @Test
     void delete() {
-        storage.delete(testResume[3].getUuid());
-        assertThrows(NotExistStorageException.class, () -> storage.delete(testResume[3].getUuid()));
+        storage.delete(expectedResumes[3].getUuid());
+        assertThrows(StorageException.class, () -> storage.get(expectedResumes[3].getUuid()));
+        assertEquals(storage.size(), EXPECTED_SIZE-1);
     }
 
     @Test
@@ -98,21 +97,15 @@ abstract class AbstractArrayStorageTest {
 
     @Test
     void getAll() {
-        Resume[] resumes = storage.getAll();
-        assertEquals(resumes.length, EXPECTED_SIZE);
+        Resume[] actual = storage.getAll();
+        assertEquals(actual.length, EXPECTED_SIZE);
 
-        Resume[] sorted = Arrays.copyOf(testResume, EXPECTED_SIZE);
-        Arrays.sort(resumes);
-        Arrays.sort(sorted);
-        assertEquals(Arrays.compare(resumes, sorted), 0);
+        Resume[] expected = Arrays.copyOf(expectedResumes, EXPECTED_SIZE);
+        assertArrayEquals(actual, expected);
     }
 
     @Test
     void size() {
         assertEquals(EXPECTED_SIZE, storage.size());
-        storage.save(new Resume());
-        assertEquals(EXPECTED_SIZE + 1, storage.size());
-        storage.save(new Resume());
-        assertEquals(EXPECTED_SIZE + 2, storage.size());
     }
 }
