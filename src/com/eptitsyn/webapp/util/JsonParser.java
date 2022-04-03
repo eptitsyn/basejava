aaefdaac1edff117df8557d94e522b4ef55d6207
Evgenii Ptitsyn <eptitsyn@gmail.com>
2022-04-04 01:55:43 +0300

    Json parser

---
+++ src/com/eptitsyn/webapp/util/JsonParser.java
@@ -0,0 +1,24 @@
+package com.eptitsyn.webapp.util;
+
+import com.eptitsyn.webapp.model.AbstractSection;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import sun.swing.SwingUtilities2;
+
+import java.io.Reader;
+import java.io.Writer;
+
+public class JsonParser {
+    private static final Gson GSON = new GsonBuilder()
+            .registerTypeAdapter(AbstractSection.class, new JsonSectionAdapter<>())
+            .setPrettyPrinting()
+            .create();
+
+    public static <T> T read(Reader reader, Class<T> clazz) {
+        return GSON.fromJson(reader, clazz);
+    }
+
+    public static <T> void write(T object, Writer writer) {
+        GSON.toJson(object, writer);
+    }
+}
