8f8c1941e4e1785bd67478df64007406fa097bdb
Evgenii Ptitsyn <eptitsyn@gmail.com>
2022-04-04 10:06:38 +0300

    Json parser fixed map key serialization

--- src/com/eptitsyn/webapp/util/JsonParser.java
+++ src/com/eptitsyn/webapp/util/JsonParser.java
@@ -1,16 +1,26 @@
 package com.eptitsyn.webapp.util;
 
 import com.eptitsyn.webapp.model.AbstractSection;
+import com.eptitsyn.webapp.model.ContactType;
+import com.eptitsyn.webapp.model.SectionType;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import sun.swing.SwingUtilities2;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.EnumMap;
 
 public class JsonParser {
     private static final Gson GSON = new GsonBuilder()
             .registerTypeAdapter(AbstractSection.class, new JsonSectionAdapter<>())
+            .enableComplexMapKeySerialization()
             .setPrettyPrinting()
             .create();
 
@@ -21,4 +31,5 @@ public class JsonParser {
     public static <T> void write(T object, Writer writer) {
         GSON.toJson(object, writer);
     }
+
 }
