37702fda8591d0aaa22c8e245711e9229c2de77b
Evgenii Ptitsyn <eptitsyn@gmail.com>
2022-05-10 23:22:20 +0300

    HW15 servlet

--- src/com/eptitsyn/webapp/storage/SqlStorage.java
+++ src/com/eptitsyn/webapp/storage/SqlStorage.java
@@ -236,5 +236,4 @@ public class SqlStorage implements Storage {
       }
     }
   }
-
 }
--- src/com/eptitsyn/webapp/web/ResumeServlet.java
+++ src/com/eptitsyn/webapp/web/ResumeServlet.java
@@ -1,5 +1,9 @@
 package com.eptitsyn.webapp.web;
 
+import com.eptitsyn.webapp.model.Resume;
+import com.eptitsyn.webapp.storage.SqlStorage;
+import com.eptitsyn.webapp.storage.Storage;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,19 +12,25 @@ import java.io.IOException;
 
 public class ResumeServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
-    }
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    Storage storage = new SqlStorage("jdbc:postgresql://localhost:5432/resumes", "postgres", "postgres");
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    request.setCharacterEncoding("UTF-8");
+    response.setCharacterEncoding("UTF-8");
+    response.setContentType("text/html; charset=UTF-8");
+    response.getWriter().write("<html><head></head><body><table><tr><th>UUID</th><th>Full Name</th></tr>");
 
+    for (Resume r : storage.getAllSorted()) {
+      response.getWriter().write("<tr><td>" + r.getUuid() + "</td><td>" + r.getFullName() + "</td><tr>");
     }
+    response.getWriter().write("</table></body></html>");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+
+  }
 }
