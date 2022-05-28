f5cc10c429c6a342a6460045832c74a6ab8cfb8c
Evgenii Ptitsyn <eptitsyn@gmail.com>
2022-05-28 18:02:27 +0300

    HW17

--- src/com/eptitsyn/webapp/Config.java
+++ src/com/eptitsyn/webapp/Config.java
@@ -3,15 +3,13 @@ package com.eptitsyn.webapp;
 import com.eptitsyn.webapp.storage.SqlStorage;
 import com.eptitsyn.webapp.storage.Storage;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.util.Properties;
 
 public class Config {
 
-  protected static final File PROPERTIES = new File(getHomeDir(), "config/resumes.properties");
+  protected static final String PROPERTIES = "/resumes.properties";
   private static final Config INSTANCE = new Config();
 
   private final Storage sqlStorage;
@@ -19,11 +17,11 @@ public class Config {
   private final String storageDir;
 
   private Config() {
-    try (InputStream is = Files.newInputStream(PROPERTIES.toPath())) {
+    try (InputStream is = Config.class.getResourceAsStream(PROPERTIES)) {
       props.load(is);
       storageDir = props.getProperty("storage.dir");
     } catch (IOException e) {
-      throw new IllegalStateException("Invalid config file " + PROPERTIES.getAbsolutePath());
+      throw new IllegalStateException("Invalid config file " + PROPERTIES);
     }
     sqlStorage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"),
             props.getProperty("db.password"));
@@ -31,15 +29,6 @@ public class Config {
 
   public static Config get() {
     return INSTANCE;
-  }
-
-  private static String getHomeDir() {
-    String homeDir = System.getProperty("homeDir");
-    File file = new File(homeDir == null ? "." : homeDir);
-    if (!file.isDirectory()) {
-      throw new IllegalStateException(homeDir + "is not directory");
-    }
-    return homeDir;
   }
 
   public String getDbPassword() {
